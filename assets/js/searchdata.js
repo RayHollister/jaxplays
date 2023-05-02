fb82c3b2b8be0336f1fa35c9744db034523b9507
Ray Hollister <git@rayhollister.com>
2023-05-02 19:38:24 -0400

    Updated Search to show Layout and Title

---
+++ assets/js/searchdata.js
@@ -0,0 +1,61 @@
+---
+layout: null
+is_wiki_page: false
+---
+{% if site.search_engine == "js" %}
+var jsondata=[
+  {% for post in site.posts %}
+    {
+      "title"    : "{{ post.title | escape }}",
+      "layout"   : "News",
+      "year"     : "{{ post.year }}",
+      "category" : "{{ post.category }}",
+      "tags"     : "{{ post.tags | join: ', ' }}",
+      "url"      : "{{ post.url }}",
+      "date"     : "{{ post.date | date_to_string }}",
+      "content"  : {{ page.content | jsonify }}
+    } {% unless forloop.last %},{% endunless %}
+  {% endfor %}
+  ,
+  {% for page in site.html_pages %}
+   {
+     {% assign title = page.title | default: page.name %}
+     {% case page.layout %}
+        {% when 'people' %}
+            {% assign layout = 'Person' %}
+        {% when 'productions' %}
+            {% assign layout = 'Production' %}
+        {% when 'shows' %}
+            {% assign layout = 'Show' %}
+        {% when 'theatres' %}
+            {% assign layout = 'Theatre' %}
+        {% when 'venues' %}
+            {% assign layout = 'Venue' %}
+        {% else %}
+            {% assign layout = '' %}
+     {% endcase %}
+     {% if title != nil and layout != '' %}
+        "title"    : "{{ title | escape }}",
+        "layout"   : "{{ layout }}",
+        "year"     : "{{ page.year }}",
+        "category" : "{{ page.category }}",
+        "tags"     : "{{ page.tags | join: ', ' }}",
+        "url"      : "{{ page.url }}",
+        "date"     : "{{ page.date }}",
+        "content"  : {{ page.content | jsonify }}
+     {% endif %}
+   } {% unless forloop.last %},{% endunless %}
+  {% endfor %}
+];
+
+var sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: jsondata,
+    searchResultTemplate: '<li><a href="{url}" title="{layout} - {title}">{layout} - {title}</a></li>',
+    noResultsText: 'No results found',
+    limit: 10,
+    fuzzy: false,
+    exclude: []
+  })
+{% endif %}
